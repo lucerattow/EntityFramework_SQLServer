83ee703fc5d6c863dffc2166ef54d923eb17800f
Lucas Ceratto <72511202+lucerattow@users.noreply.github.com>
2022-06-05 00:32:44 -0300

    Add project files.

---
+++ EntityFramework_SQLServer/Form1.Designer.cs
@@ -0,0 +1,247 @@
+﻿namespace EntityFramework_SQLServer
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.grd = new System.Windows.Forms.DataGridView();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.label1 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.label2 = new System.Windows.Forms.Label();
+            this.TxtNombre = new System.Windows.Forms.TextBox();
+            this.TxtApellido = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.TxtEdad = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.CbGenero = new System.Windows.Forms.ComboBox();
+            this.BtnCrear = new System.Windows.Forms.Button();
+            this.BtnModificar = new System.Windows.Forms.Button();
+            this.BtnEliminar = new System.Windows.Forms.Button();
+            this.BtnLimpiar = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.grd)).BeginInit();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // grd
+            // 
+            this.grd.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.grd.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.grd.Location = new System.Drawing.Point(206, 56);
+            this.grd.Name = "grd";
+            this.grd.Size = new System.Drawing.Size(617, 382);
+            this.grd.TabIndex = 0;
+            this.grd.CellDoubleClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.grd_CellDoubleClick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.label1);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(835, 46);
+            this.panel1.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Consolas", 18F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(207, 28);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Personas (CRUD)";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.BtnLimpiar);
+            this.panel2.Controls.Add(this.BtnEliminar);
+            this.panel2.Controls.Add(this.BtnModificar);
+            this.panel2.Controls.Add(this.BtnCrear);
+            this.panel2.Controls.Add(this.CbGenero);
+            this.panel2.Controls.Add(this.label5);
+            this.panel2.Controls.Add(this.TxtEdad);
+            this.panel2.Controls.Add(this.label4);
+            this.panel2.Controls.Add(this.TxtApellido);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Controls.Add(this.TxtNombre);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Left;
+            this.panel2.Location = new System.Drawing.Point(0, 46);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(200, 404);
+            this.panel2.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(10, 10);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 13);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Nombre";
+            // 
+            // TxtNombre
+            // 
+            this.TxtNombre.Location = new System.Drawing.Point(10, 25);
+            this.TxtNombre.Name = "TxtNombre";
+            this.TxtNombre.Size = new System.Drawing.Size(180, 20);
+            this.TxtNombre.TabIndex = 1;
+            // 
+            // TxtApellido
+            // 
+            this.TxtApellido.Location = new System.Drawing.Point(10, 63);
+            this.TxtApellido.Name = "TxtApellido";
+            this.TxtApellido.Size = new System.Drawing.Size(180, 20);
+            this.TxtApellido.TabIndex = 3;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(10, 48);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(44, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Apellido";
+            // 
+            // TxtEdad
+            // 
+            this.TxtEdad.Location = new System.Drawing.Point(10, 101);
+            this.TxtEdad.Name = "TxtEdad";
+            this.TxtEdad.Size = new System.Drawing.Size(58, 20);
+            this.TxtEdad.TabIndex = 5;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 86);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(32, 13);
+            this.label4.TabIndex = 4;
+            this.label4.Text = "Edad";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(71, 86);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(42, 13);
+            this.label5.TabIndex = 6;
+            this.label5.Text = "Genero";
+            // 
+            // CbGenero
+            // 
+            this.CbGenero.FormattingEnabled = true;
+            this.CbGenero.Location = new System.Drawing.Point(74, 101);
+            this.CbGenero.Name = "CbGenero";
+            this.CbGenero.Size = new System.Drawing.Size(116, 21);
+            this.CbGenero.TabIndex = 7;
+            // 
+            // BtnCrear
+            // 
+            this.BtnCrear.Location = new System.Drawing.Point(10, 128);
+            this.BtnCrear.Name = "BtnCrear";
+            this.BtnCrear.Size = new System.Drawing.Size(180, 26);
+            this.BtnCrear.TabIndex = 8;
+            this.BtnCrear.Text = "Crear";
+            this.BtnCrear.UseVisualStyleBackColor = true;
+            this.BtnCrear.Click += new System.EventHandler(this.BtnCrear_Click);
+            // 
+            // BtnModificar
+            // 
+            this.BtnModificar.Location = new System.Drawing.Point(10, 160);
+            this.BtnModificar.Name = "BtnModificar";
+            this.BtnModificar.Size = new System.Drawing.Size(180, 26);
+            this.BtnModificar.TabIndex = 9;
+            this.BtnModificar.Text = "Modificar";
+            this.BtnModificar.UseVisualStyleBackColor = true;
+            this.BtnModificar.Click += new System.EventHandler(this.BtnModificar_Click);
+            // 
+            // BtnEliminar
+            // 
+            this.BtnEliminar.Location = new System.Drawing.Point(10, 189);
+            this.BtnEliminar.Name = "BtnEliminar";
+            this.BtnEliminar.Size = new System.Drawing.Size(180, 26);
+            this.BtnEliminar.TabIndex = 10;
+            this.BtnEliminar.Text = "Eliminar";
+            this.BtnEliminar.UseVisualStyleBackColor = true;
+            this.BtnEliminar.Click += new System.EventHandler(this.BtnEliminar_Click);
+            // 
+            // BtnLimpiar
+            // 
+            this.BtnLimpiar.Location = new System.Drawing.Point(10, 366);
+            this.BtnLimpiar.Name = "BtnLimpiar";
+            this.BtnLimpiar.Size = new System.Drawing.Size(180, 26);
+            this.BtnLimpiar.TabIndex = 11;
+            this.BtnLimpiar.Text = "Limpiar";
+            this.BtnLimpiar.UseVisualStyleBackColor = true;
+            this.BtnLimpiar.Click += new System.EventHandler(this.BtnLimpiar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(835, 450);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Controls.Add(this.grd);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.grd)).EndInit();
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.DataGridView grd;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.ComboBox CbGenero;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.TextBox TxtEdad;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.TextBox TxtApellido;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox TxtNombre;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button BtnLimpiar;
+        private System.Windows.Forms.Button BtnEliminar;
+        private System.Windows.Forms.Button BtnModificar;
+        private System.Windows.Forms.Button BtnCrear;
+    }
+}
+
---
+++ EntityFramework_SQLServer/Form1.cs
@@ -0,0 +1,176 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace EntityFramework_SQLServer
+{
+    public partial class Form1 : Form
+    {
+        int? selected_id = null;
+
+        public Form1()
+        {
+            InitializeComponent();
+            SetupGrd();
+            SetupCbGenero();
+            GrdGetPersonas();
+            logicaFormulario("limpiar");
+        }
+
+        //Inicializar el formulario
+        private void SetupGrd()
+        {
+            grd.EditMode = DataGridViewEditMode.EditProgrammatically;
+
+            grd.Columns.Add(new DataGridViewColumn()
+            {
+                CellTemplate = new DataGridViewTextBoxCell(),
+                HeaderText = "id",
+                Visible = false
+            });
+            grd.Columns.Add(new DataGridViewColumn()
+            {
+                CellTemplate = new DataGridViewTextBoxCell(),
+                HeaderText = "nombre"
+            });
+            grd.Columns.Add(new DataGridViewColumn()
+            {
+                CellTemplate = new DataGridViewTextBoxCell(),
+                HeaderText = "apellido"
+            });
+            grd.Columns.Add(new DataGridViewColumn()
+            {
+                CellTemplate = new DataGridViewTextBoxCell(),
+                HeaderText = "edad"
+            });
+            grd.Columns.Add(new DataGridViewColumn()
+            {
+                CellTemplate = new DataGridViewTextBoxCell(),
+                HeaderText = "genero"
+            });
+        }
+        private void SetupCbGenero()
+        {
+            CbGenero.DisplayMember = "nombre";
+            CbGenero.ValueMember = "id";
+
+            using (PruebaEntities db = new PruebaEntities())
+            {
+                var a = db.Persona_Genero.ToList();
+                CbGenero.DataSource = a;
+            }
+        }
+
+        //Metodos
+        private void GrdGetPersonas()
+        {
+            grd.Rows.Clear();
+
+            using (PruebaEntities db = new PruebaEntities())
+            {
+                foreach (var item in db.Persona)
+                    grd.Rows.Add(item.id, item.nombre, item.apellido, item.edad, item.Persona_Genero.nombre);
+            }
+        }
+        private void logicaFormulario(string accion)
+        {
+            switch (accion)
+            {
+                case "editar":
+                    BtnCrear.Enabled = false;
+                    BtnModificar.Enabled = true;
+                    BtnEliminar.Enabled = true;
+                    BtnLimpiar.Enabled = true;
+                    break;
+
+                case "limpiar":
+                    selected_id = null;
+
+                    TxtNombre.Text = "";
+                    TxtApellido.Text = "";
+                    TxtEdad.Text = "";
+                    CbGenero.SelectedIndex = 0;
+
+                    BtnCrear.Enabled = true;
+                    BtnModificar.Enabled = false;
+                    BtnEliminar.Enabled = false;
+                    BtnLimpiar.Enabled = false;
+                    break;
+            }
+        }
+
+        //Eventos
+        private void grd_CellDoubleClick(object sender, DataGridViewCellEventArgs e)
+        {
+            using(PruebaEntities db = new PruebaEntities())
+            {
+                Persona o = db.Persona.Find(grd.Rows[e.RowIndex].Cells[0].Value);
+
+                selected_id = o.id;
+                TxtNombre.Text = o.nombre;
+                TxtApellido.Text = o.apellido;
+                TxtEdad.Text = o.edad.ToString();
+                CbGenero.SelectedValue = o.Persona_Genero.id;
+
+                logicaFormulario("editar");
+            }
+        }
+        private void BtnLimpiar_Click(object sender, EventArgs e)
+        {
+            logicaFormulario("limpiar");
+        }
+        private void BtnCrear_Click(object sender, EventArgs e)
+        {
+            using (PruebaEntities db = new PruebaEntities())
+            {
+                Persona o = new Persona();
+                o.nombre = TxtNombre.Text;
+                o.apellido = TxtApellido.Text;
+                o.edad = int.Parse(TxtEdad.Text);
+                o.Persona_Genero = db.Persona_Genero.Find(((Persona_Genero)CbGenero.SelectedItem).id);
+
+                db.Persona.Add(o);
+                db.SaveChanges();
+
+                GrdGetPersonas();
+                logicaFormulario("limpiar");
+            }
+        }
+        private void BtnModificar_Click(object sender, EventArgs e)
+        {
+            using (PruebaEntities db = new PruebaEntities())
+            {
+                Persona o = db.Persona.Find(selected_id);
+                o.nombre = TxtNombre.Text;
+                o.apellido = TxtApellido.Text;
+                o.edad = int.Parse(TxtEdad.Text);
+                o.Persona_Genero = db.Persona_Genero.Find(((Persona_Genero)CbGenero.SelectedItem).id);
+
+                db.Entry(o).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+
+                GrdGetPersonas();
+                logicaFormulario("limpiar");
+            }
+        }
+        private void BtnEliminar_Click(object sender, EventArgs e)
+        {
+            using (PruebaEntities db = new PruebaEntities())
+            {
+                Persona o = db.Persona.Find(selected_id);
+
+                db.Persona.Remove(o);
+                db.SaveChanges();
+
+                GrdGetPersonas();
+                logicaFormulario("limpiar");
+            }
+        }
+    }
+}
